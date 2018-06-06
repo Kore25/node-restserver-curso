e14760a310aec2bf39664e379c2412a927a27250
“Luis” <luis.flores@inmediatum.com>
2018-06-06 16:50:37 -0500

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -10,7 +10,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:Fuerza2018@ds145325.mlab.com:45325/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URL_DB = urlDB;
